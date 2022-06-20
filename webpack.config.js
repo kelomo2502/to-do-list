8af27447b7f1badf360af30aca17473d73a1f537
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-06-20 11:30:24 +0100

    rename doc to docs

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './doc/index.js',
+  entry: './docs/index.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -27,7 +27,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack exercise',
-      template: './doc/index.html',
+      template: './docs/index.html',
     }),
   ],
 };
